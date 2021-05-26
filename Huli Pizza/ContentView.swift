9539e2e5d6e52a54246849537263bfd70b210571
Kevin Tran <kevin2005tran@gmail.com>
2021-05-25 20:03:35 -0700

    Initial Commit

---
+++ Huli Pizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Huli Pizza
+//
+//  Created by Kevin Tran on 5/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Huli Pizza/Huli_PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Huli_PizzaApp.swift
+//  Huli Pizza
+//
+//  Created by Kevin Tran on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Huli_PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
